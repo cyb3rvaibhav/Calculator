bf294eb8924b78738dca974499c775ca27219222
cyb3rvaibhav <cyb3rvaibhav@gmail.com>
2024-06-21 19:17:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './app.css'
+function App(){
+    return (
+        <>
+        <div className="circle"/>
+        <div className="black-box">
+            <div className="keys">
+                <div className="row1">
+                </div>
+            </div>
+        </div>
+      
+        <div className="white-box">
+            <div className="keys keys-white"></div>
+        </div>
+         </>
+ )
+}
+export default App;
